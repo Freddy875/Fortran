827a1b672d990802176618ca20333a418309d8be
Freddy875 <fer875nandosm@gmail.com>
2020-07-16 19:43:27 -0500

    Creamos nuestro primer prorama en Fortran

---
+++ Ejemplo1.f90
@@ -0,0 +1,22 @@
+program ejemplo1 
+
+    implicit none 
+
+    integer::a,b,c 
+    real::e,f,g 
+
+    a=10
+    b=20
+    c=a+b 
+
+    e=10.22
+    f=8.79
+    g=e+f 
+
+    print *, "La suma de los enteros es: ",c 
+
+    print *, "La suma de los reales  es: ",c 
+
+    pause 
+
+end program ejemplo1   
